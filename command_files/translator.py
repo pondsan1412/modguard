dc4a93d2b04c17e58b1b53465aafa606381d54a8
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2024-07-11 12:05:58 +0000

    I add paper.py

--- command_files/translator.py
+++ command_files/translator.py
@@ -5,9 +5,7 @@ class translator(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command(name='test')
-    async def test(self,variables:commands.Context):
-        await variables.send('hello world')
+    
 
 
 async def setup(bot):
