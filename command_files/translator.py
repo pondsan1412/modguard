171ca86cbac59615ddd05fcb0ce05a7ab1a33ccf
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2024-07-11 12:16:24 +0000

    "start write function"

--- command_files/translator.py
+++ command_files/translator.py
@@ -5,8 +5,10 @@ class translator(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self.bot = bot
 
-    
-
+    async def monitoring_user(self,msg:discord.Message)-> bool:
+        pass
+    async def tracking_message(self,msg:discord.Message):
+        pass
 
 async def setup(bot):
     await bot.add_cog(translator(bot))
