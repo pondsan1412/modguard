352b22be206ff87830cc15628c1db35bbfed14c8
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2024-07-12 04:26:40 +0000

    None

--- command_files/translator.py
+++ command_files/translator.py
@@ -5,13 +5,10 @@ class translator(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self.bot = bot
 
-    async def monitoring_user(self,msg:discord.Message)-> bool:
-        pass
-    async def tracking_message(self,msg:discord.Message):
-        pass
     async def check_bool()-> bool:
         return False
     async def callback_user_id(self) ->int:
         return 0
+    
 async def setup(bot):
     await bot.add_cog(translator(bot))
