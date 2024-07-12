96ed914bfa1fd1b9b5b1998089d776ca423570a3
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2024-07-12 06:54:14 +0000

    "make admin commands"

--- command_files/translator.py
+++ command_files/translator.py
@@ -1,14 +1,26 @@
 import discord
 from discord.ext import commands
+from discord import app_commands,Embed
+from easygoogletranslate import EasyGoogleTranslate
+import discord.utils
 
-class translator(commands.Cog):
-    def __init__(self,bot:commands.Bot):
+class context(commands.Cog):
+    def __init__(self, bot:commands.Bot) -> None:
         self.bot = bot
+        self.ctx_menu = app_commands.ContextMenu(
+            name="Translate",
+            callback=self.tl,
+        )
+        self.bot.tree.add_command(self.ctx_menu)
 
-    async def check_bool()-> bool:
-        return False
-    async def callback_user_id(self) ->int:
-        return 0
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
+
+    async def tl(self,i:discord.Interaction,msg:discord.Message):
+        await i.response.send_message('hi')
+    
     
 async def setup(bot):
-    await bot.add_cog(translator(bot))
+    await bot.add_cog(
+        context(bot)
+    )
