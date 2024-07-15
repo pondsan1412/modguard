c235ad922fb88a87d7070d66a437cd70a27b7c8a
pondsan1412 <pondsan1412@gmail.com>
2024-07-15 16:32:39 +0700

    everything fixed

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-from modules import variables as v
-import requests
-import json
+import aiohttp
+import asyncio
 
-def fetch_player_data(player_name):
-    url = v.mk8dx_api_url_name + player_name
-    respone = requests.get(url)
-    if respone.status_code == 200:
-        player_data = respone.json()
-        player_data["discordId"]
-        return player_data["discordId"]
-    
-print(fetch_player_data(player_name='nissan'))
+async def find_player_names():
+    url = 'https://www.mk8dx-lounge.com/api/player/list'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                player_data = await response.json()
+                for player in player_data['players']:
+                    if player['name'] == 'Sky_2k':
+                        print(player['discordId'])
+                        
+asyncio.run(find_player_names())
