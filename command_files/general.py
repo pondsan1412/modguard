96d66332d8b6e3e525d6ab063f323f391ee7c443
pondsan1412 <pondsan1412@gmail.com>
2024-07-17 17:08:07 +0700

    add general command

---
+++ command_files/general.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import shlex
+import random
+import asyncio
+from modules import variables as v
+
+class General(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def random_things(self, ctx: commands.Context, *, things: str):
+        """Random anything you want sorted by ',' or '|' """
+
+        if "|" in things:
+            delimiter = "|"
+        elif "," in things:
+            delimiter = ","
+        else:
+            delimiter = None
+
+        if delimiter is not None:
+            thing = things.split(delimiter)
+        else:
+            thing = shlex.split(things)
+
+        random_thing = random.choice(thing)  # Choose randomly from the list of things
+
+        def random_time():
+            time_ = [ 2, 3, 4, 5,6]
+            return random.choice(time_)
+
+        if ctx:
+            message = await ctx.send(content=f'{v.loading_system}')
+            await asyncio.sleep(random_time())
+            await message.edit(content=f'{random_thing}')
+
+async def setup(bot):
+   await bot.add_cog(General(bot))
